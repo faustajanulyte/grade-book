6835d8a9a0b164a3646c960e43606cc713616be8
JANULYTE Fausta <fjanulyte@one.steria.dom>
2019-11-06 16:09:45 +0000

    finished

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,17 +7,31 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-        
-            var book = new Book("Fausta's Grade Book");
 
-            while(true)
+            IBook book = new DiskBook("Fausta's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            System.Console.WriteLine($"For the book names {book.Name}");
+            System.Console.WriteLine($"The lowest grade is {stats.Low}");
+            System.Console.WriteLine($"The highest grade is {stats.High}");
+            System.Console.WriteLine($"The average grade is {stats.Average}");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit");
                 var input = Console.ReadLine();
 
-                if(input == "q")
+                if (input == "q")
                 {
-                   break;
+                    break;
                 }
 
                 try
@@ -25,11 +39,11 @@ namespace GradeBook
                     var grade = double.Parse(input);
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex)
+                catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
@@ -38,13 +52,11 @@ namespace GradeBook
                     Console.WriteLine("**");
                 }
             }
+        }
 
-            var stats = book.GetStatistics();
-
-            System.Console.WriteLine($"The lowest grade is {stats.Low}");
-            System.Console.WriteLine($"The highest grade is {stats.High}");
-            System.Console.WriteLine($"The average grade is {stats.Average}");
-            System.Console.WriteLine($"The letter grade is {stats.Letter}");
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("A grade was added");
         }
     }
 }
